0fecd085af42a769e5d68578ff746df2512f3440
Marco <bigdatajang@gmail.com>
2023-06-27 21:09:46 +0900

    feat: Mapped Types 을 사용해서 entity를 통해 dto 생성

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,8 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -10,18 +12,23 @@ export class Restaurant {
 
   @Field((returns) => String)
   @Column()
+  @IsString()
+  @Length(5, 10)
   name: string;
 
   @Field((returns) => Boolean, { nullable: true })
   @Column()
+  @IsBoolean()
   isGood?: boolean;
 
   @Field((returns) => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field((returns) => String)
   @Column()
+  @IsString()
   ownerName: string;
 
   @Field((returns) => String)
