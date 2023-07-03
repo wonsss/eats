9903fd7b70acb652e86866ca542d24b87752cd68
Marco <bigdatajang@gmail.com>
2023-07-04 00:37:04 +0900

    feat: Account Mutation 추가

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,8 +1,40 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
 
 @Resolver((of) => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
+
+  @Query((returns) => Boolean)
+  hi() {
+    return true;
+  }
+
+  @Mutation((of) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.usersService.createAccount(createAccountInput);
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      }
+      return {
+        ok: true,
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
 }
