12f7ae9b1bb8adb535f3335ad22315fc84aaa1e6
Marco <bigdatajang@gmail.com>
2023-07-03 23:49:13 +0900

    feat: User Resolver와 Service 추가

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 backend
 
+http://127.0.0.1:3000/graphql
+
 ## User Model:
 
 - id
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,8 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver((of) => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+}
