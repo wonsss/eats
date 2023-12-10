c647bdb00d6573dcf7554d2417da99baa692603c
Marco <bigdatajang@gmail.com>
2023-12-10 20:26:33 +0900

    test: should fail if user exists 테스트 작성

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -7,11 +7,11 @@ import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
 import { Repository } from 'typeorm';
 
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 
 const mockJwtService = {
   sign: jest.fn(),
@@ -27,6 +27,8 @@ type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>; 
 describe('UserService', () => {
   let service: UserService;
   let usersRepository: MockRepository<User>;
+  let verificationsRepository: MockRepository<Verification>;
+  let emailService: MailService;
 
   beforeAll(async () => {
     // 역할: 테스트 모듈을 만들어서 테스트 모듈을 통해 테스트를 진행한다.
@@ -35,11 +37,11 @@ describe('UserService', () => {
         UserService,
         {
           provide: getRepositoryToken(User), // User Repository를 Mock Repository로 대체한다.
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: getRepositoryToken(Verification), // Verification Repository를 Mock Repository로 대체한다.
-          useValue: mockRepository,
+          useValue: mockRepository(),
         },
         {
           provide: JwtService, // JwtService를 Mock Service로 대체한다.
@@ -53,6 +55,8 @@ describe('UserService', () => {
     }).compile(); // 테스트 모듈을 컴파일한다.
     service = module.get<UserService>(UserService); // 테스트 모듈에서 UserService를 가져온다.
     usersRepository = module.get(getRepositoryToken(User)); // 테스트 모듈에서 User Repository를 가져온다.
+    verificationsRepository = module.get(getRepositoryToken(Verification)); // 테스트 모듈에서 Verification Repository를 가져온다.
+    emailService = module.get<MailService>(MailService); // 테스트 모듈에서 MailService를 가져온다.
   });
 
   it('should be defined', () => {
@@ -60,20 +64,57 @@ describe('UserService', () => {
   });
 
   describe('createAccount', () => {
+    const createAccountArgs = {
+      email: 'test@email.com',
+      password: 'test_password',
+      role: 0,
+    };
+
     it('should fail if user exists', async () => {
       usersRepository.findOne.mockResolvedValue({
         id: 1,
         email: '',
       });
-      const result = await service.createAccount({
-        email: '',
-        password: '',
-        role: 0,
-      });
+      const result = await service.createAccount(createAccountArgs);
       expect(result).toMatchObject({
         ok: false,
         error: 'There is a user with that email already',
       });
+    });
+
+    it('should create a new user', async () => {
+      // return value를 mocking한다.
+      usersRepository.findOne.mockResolvedValue(undefined); // findOne이 undefined를 반환하도록 설정한다.
+      usersRepository.create.mockReturnValue(createAccountArgs); // create가 createAccountArgs를 반환하도록 설정한다.
+      usersRepository.save.mockResolvedValue(createAccountArgs); // save가 createAccountArgs를 반환하도록 설정한다.
+      verificationsRepository.create.mockReturnValue(createAccountArgs); // create가 createAccountArgs를 반환하도록 설정한다.
+      verificationsRepository.save.mockResolvedValue({ code: 'code' }); // save가 createAccountArgs를 반환하도록 설정한다.
+
+      const result = await service.createAccount(createAccountArgs); // createAccount를 실행한다.
+
+      expect(usersRepository.create).toHaveBeenCalledTimes(1); // create가 1번 실행되었어야 한다.
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs); // create가 createAccountArgs를 인자로 받아 실행되었어야 한다.
+
+      expect(usersRepository.save).toHaveBeenCalledTimes(1); // save가 1번 실행되었어야 한다.
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs); // save가 createAccountArgs를 인자로 받아 실행되었어야 한다.
+
+      expect(verificationsRepository.create).toHaveBeenCalledTimes(1);
+      expect(verificationsRepository.create).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(verificationsRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationsRepository.save).toHaveBeenCalledWith(
+        createAccountArgs,
+      );
+
+      expect(emailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(emailService.sendVerificationEmail).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.any(String),
+      ); // sendVerificationEmail가 String 타입의 인자 2개를 받아 실행되었어야 한다.
+
+      expect(result).toEqual({ ok: true }); // createAccount의 결과는 { ok: true }이어야 한다.
     });
   });
 });
