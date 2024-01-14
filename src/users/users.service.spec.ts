f2e33b4a26bab3375cd6315c51084300e697f98f
Marco <bigdatajang@gmail.com>
2024-01-14 20:08:15 +0900

    test: edit profile

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -14,14 +14,14 @@ const mockRepository = () => ({
   findOneOrFail: jest.fn(),
 });
 
-const mockJwtService = {
+const mockJwtService = () => ({
   sign: jest.fn(() => 'signed-token'), // sign이 'signed-token'을 반환하도록 설정한다.
   verify: jest.fn(),
-};
+});
 
-const mockMailService = {
+const mockMailService = () => ({
   sendVerificationEmail: jest.fn(),
-};
+});
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>; // 타입 의미: Repository의 모든 키를 가져와서 그 키들을 Record의 키로 만들고, Record의 값은 jest.Mock로 만든다.
 
@@ -29,7 +29,7 @@ describe('UserService', () => {
   let service: UserService;
   let usersRepository: MockRepository<User>;
   let verificationsRepository: MockRepository<Verification>;
-  let emailService: MailService;
+  let mailService: MailService;
   let jwtService: JwtService;
 
   beforeEach(async () => {
@@ -47,18 +47,18 @@ describe('UserService', () => {
         },
         {
           provide: JwtService, // JwtService를 Mock Service로 대체한다.
-          useValue: mockJwtService,
+          useValue: mockJwtService(),
         },
         {
           provide: MailService, // MailService를 Mock Service로 대체한다.
-          useValue: mockMailService,
+          useValue: mockMailService(),
         },
       ],
     }).compile(); // 테스트 모듈을 컴파일한다.
     service = module.get<UserService>(UserService); // 테스트 모듈에서 UserService를 가져온다.
     usersRepository = module.get(getRepositoryToken(User)); // 테스트 모듈에서 User Repository를 가져온다.
     verificationsRepository = module.get(getRepositoryToken(Verification)); // 테스트 모듈에서 Verification Repository를 가져온다.
-    emailService = module.get<MailService>(MailService); // 테스트 모듈에서 MailService를 가져온다.
+    mailService = module.get<MailService>(MailService); // 테스트 모듈에서 MailService를 가져온다.
     jwtService = module.get<JwtService>(JwtService); // 테스트 모듈에서 JwtService를 가져온다.
   });
 
@@ -111,8 +111,8 @@ describe('UserService', () => {
         createAccountArgs,
       );
 
-      expect(emailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
-      expect(emailService.sendVerificationEmail).toHaveBeenCalledWith(
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
         expect.any(String),
         expect.any(String),
       ); // sendVerificationEmail가 String 타입의 인자 2개를 받아 실행되었어야 한다.
@@ -197,6 +197,72 @@ describe('UserService', () => {
       usersRepository.findOneOrFail.mockRejectedValue(new Error());
       const result = await service.findById(findByIdArgs.id);
       expect(result).toEqual({ ok: false, error: 'User Not Found' });
+    });
+  });
+
+  describe('editProfile', () => {
+    it('should change email', async () => {
+      const oldUser = {
+        email: 'bs@old.com',
+        verified: true,
+      };
+      const editProfileArgs = {
+        userId: 1,
+        input: { email: 'bs@new.com' },
+      };
+      const newVerification = {
+        code: 'code',
+      };
+      const newUser = {
+        email: editProfileArgs.input.email,
+        verified: false,
+      };
+
+      usersRepository.findOne.mockResolvedValue(oldUser);
+      verificationsRepository.create.mockReturnValue(newVerification);
+      verificationsRepository.save.mockResolvedValue(newVerification);
+
+      await service.editProfile(editProfileArgs.userId, editProfileArgs.input);
+
+      expect(usersRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(usersRepository.findOne).toHaveBeenCalledWith({
+        where: { id: editProfileArgs.userId },
+      });
+      expect(verificationsRepository.create).toHaveBeenCalledWith({
+        user: newUser,
+      });
+      expect(verificationsRepository.save).toHaveBeenCalledWith(
+        newVerification,
+      );
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        newUser.email,
+        newVerification.code,
+      );
+    });
+
+    it('should change password', async () => {
+      const editProfileArgs = {
+        userId: 1,
+        input: { password: 'new.password' },
+      };
+
+      usersRepository.findOne.mockResolvedValue({ password: 'old' });
+      const result = await service.editProfile(
+        editProfileArgs.userId,
+        editProfileArgs.input,
+      );
+
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(editProfileArgs.input);
+      expect(result).toEqual({ ok: true });
+    });
+
+    it('should fail on exception', async () => {
+      usersRepository.findOne.mockRejectedValue(new Error());
+      const result = await service.editProfile(1, { email: '12' });
+
+      expect(result).toEqual({ ok: false, error: 'Could not update profile.' });
     });
   });
 });
