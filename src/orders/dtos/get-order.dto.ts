e391e3af159d9375344e1b7e30b54fe9af0b7ddd
Marco <bigdatajang@gmail.com>
2024-04-10 16:07:22 +0900

    fix: InputType에 dummy 추가

--- src/orders/dtos/get-order.dto.ts
+++ src/orders/dtos/get-order.dto.ts
@@ -3,7 +3,10 @@ import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Order } from '../entities/order.entity';
 
 @InputType()
-export class GetOrderInput extends PickType(Order, ['id']) {}
+export class GetOrderInput extends PickType(Order, ['id']) {
+  @Field((type) => Boolean)
+  _dummy = true;
+}
 
 @ObjectType()
 export class GetOrderOutput extends CoreOutput {
